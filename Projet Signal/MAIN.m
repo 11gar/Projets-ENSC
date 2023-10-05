fcb63568ecb72a87734eb58add71728e420f9a08
Ansgar <wetzel.ansgar@gmail.com>
2023-10-05 09:46:14 +0200

    cc

--- Projet Signal/MAIN.m
+++ Projet Signal/MAIN.m
@@ -4,10 +4,7 @@ vidObj = VideoReader("vidéo.mp4");
 img= read(vidObj,1);
 %img=img(500:700,600:800,:);
 %img=img(20:40,120:140,:);
-A=img(:,:,3);
-contours = trouvercontours(A);
-
-B=binarize(contours,2).*255;
+[imgIndex,map]=rgb2ind(img,255);
 %over=overline(A,1,1,1080,1920);
 
 
@@ -15,13 +12,13 @@ B=binarize(contours,2).*255;
 briqueBleue=findexclusivecolor(img,38,60,174,50,50,50);
 briqueBlanche=findexclusivecolor(img,210,210,210,70,70,70);
 
+[indexBleue,map]=rgb2ind(briqueBleue,255);
+[X,Y,norme] = trouvercontours(indexBleue);
+normeBin=binarize(norme,10);
+
 %briqueBleue=convolutionCouleurs(briqueBleue,"same");
 
-
-
-
 figure(1),image(img);
-figure(2), image(B);
+figure(2), imagesc(normeBin), colormap("jet");
 figure(3), imagesc(briqueBleue),colormap("jet");
-figure(4), imagesc(briqueBleue),colormap("jet");
 %figure(5),image(over),colormap("jet");
