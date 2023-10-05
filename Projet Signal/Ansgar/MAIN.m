8e037bad0a607c55cc7a080c3cd91de4bcacef1f
Ansgar <wetzel.ansgar@gmail.com>
2023-10-05 10:52:37 +0200

    wew

--- Projet Signal/Ansgar/MAIN.m
+++ Projet Signal/Ansgar/MAIN.m
@@ -9,16 +9,26 @@ img= read(vidObj,1);
 
 
 
-briqueBleue=findexclusivecolor(img,38,60,174,50,50,50);
+briqueBleue=findexclusivecolor(img,38,60,174,20,20,20);
 briqueBlanche=findexclusivecolor(img,210,210,210,70,70,70);
 
 [indexBleue,map]=rgb2ind(briqueBleue,255);
 [X,Y,norme] = trouvercontours(indexBleue);
-normeBin=binarize(norme,10);
+normeBin=binarize(norme,20);
 
 %briqueBleue=convolutionCouleurs(briqueBleue,"same");
 
-figure(1),image(img);
-figure(2), imagesc(normeBin), colormap("jet");
+
+figure(2), imagesc(normeBin), colormap("gray");
 figure(3), imagesc(briqueBleue),colormap("jet");
 %figure(5),image(over),colormap("jet");
+figure(1),imshow(img);
+[x,y]=ginput(2);
+xmid=(x(1)+x(2))/2;
+ymid=(y(1)+y(2))/2;
+vect=[(x(2)-x(1))/(sqrt((x(2)-x(1))^2+(y(2)-y(1))^2)),(y(2)-y(1))/(sqrt((x(2)-x(1))^2+(y(2)-y(1))^2))].*30
+ortho=null(vect(:).').*50;
+vect*ortho
+line(x,y);
+line([xmid,xmid-ortho(1)],[ymid,ymid-ortho(2)],'color','red')
+line([x(1),x(1)+vect(1)],[y(1),y(1)+vect(2)],'color','red')
--- Projet Signal/Ansgar/trouvercontours.m
+++ Projet Signal/Ansgar/trouvercontours.m
@@ -1,8 +1,8 @@
 function[Gx,Gy,norme] = trouvercontours(matrix)
-sigma=0.5;
+sigma=1;
 [X,Y]=meshgrid(-sigma*3-0.5:0.5+sigma*3);
 Hx=-X.*exp(-(X.^2+Y.^2)/(2*sigma^2));
 Hy=-Y.*exp(-(X.^2+Y.^2)/(2*sigma^2));
 Gx=conv2(matrix,Hx,'same');
 Gy=conv2(matrix,Hy,'same');
-norme=(Gx.*Gx+Gy.*Gy).^0.5;
+norme=((Gx.*Gx+Gy.*Gy).^0.5)/sigma;
